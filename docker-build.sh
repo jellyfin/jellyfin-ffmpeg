b3a37870d45bbfbf8d1b6e5605a5a7fbc25847bc
nyanmisaka <nst799610810@gmail.com>
2022-09-02 17:47:45 +0800

    Update dependencies

--- docker-build.sh
+++ docker-build.sh
@@ -153,7 +153,7 @@ prepare_extra_amd64() {
     pushd ${SOURCE_DIR}
     mkdir libdrm
     pushd libdrm
-    libdrm_ver="2.4.112"
+    libdrm_ver="2.4.113"
     libdrm_link="https://dri.freedesktop.org/libdrm/libdrm-${libdrm_ver}.tar.xz"
     wget ${libdrm_link} -O libdrm.tar.xz
     tar xaf libdrm.tar.xz
@@ -161,8 +161,9 @@ prepare_extra_amd64() {
         --prefix=${TARGET_DIR} \
         --libdir=lib \
         --buildtype=release \
-        -D{amdgpu,radeon,intel,udev}=true \
-        -D{valgrind,freedreno,vc4,vmwgfx,nouveau,man-pages}=false
+        -D{udev,tests,install-test-programs}=false \
+        -D{amdgpu,radeon,intel}=enabled \
+        -D{valgrind,freedreno,vc4,vmwgfx,nouveau,man-pages}=disabled
     meson configure drm_build
     ninja -C drm_build install
     cp ${TARGET_DIR}/lib/libdrm*.so* ${SOURCE_DIR}/libdrm
@@ -230,7 +231,7 @@ prepare_extra_amd64() {
     # Provides MSDK runtime (libmfxhw64.so.1) for 11th Gen Rocket Lake and older
     # Provides MFX dispatcher (libmfx.so.1) for FFmpeg
     pushd ${SOURCE_DIR}
-    git clone -b intel-mediasdk-22.5.2 --depth=1 https://github.com/Intel-Media-SDK/MediaSDK
+    git clone -b intel-mediasdk-22.5.3 --depth=1 https://github.com/Intel-Media-SDK/MediaSDK
     pushd MediaSDK
     sed -i 's|MFX_PLUGINS_CONF_DIR "/plugins.cfg"|"/usr/lib/jellyfin-ffmpeg/lib/mfx/plugins.cfg"|g' api/mfx_dispatch/linux/mfxloader.cpp
     mkdir build && pushd build
@@ -250,7 +251,7 @@ prepare_extra_amd64() {
     # Provides VPL runtime (libmfx-gen.so.1.2) for 11th Gen Tiger Lake and newer
     # Both MSDK and VPL runtime can be loaded by MFX dispatcher (libmfx.so.1)
     pushd ${SOURCE_DIR}
-    git clone -b intel-onevpl-22.5.2 --depth=1 https://github.com/oneapi-src/oneVPL-intel-gpu
+    git clone -b intel-onevpl-22.5.3 --depth=1 https://github.com/oneapi-src/oneVPL-intel-gpu
     pushd oneVPL-intel-gpu
     mkdir build && pushd build
     cmake -DCMAKE_INSTALL_PREFIX=${TARGET_DIR} ..
@@ -264,7 +265,7 @@ prepare_extra_amd64() {
     # Full Feature Build: ENABLE_KERNELS=ON(Default) ENABLE_NONFREE_KERNELS=ON(Default)
     # Free Kernel Build: ENABLE_KERNELS=ON ENABLE_NONFREE_KERNELS=OFF
     pushd ${SOURCE_DIR}
-    git clone -b intel-media-22.5.2 --depth=1 https://github.com/intel/media-driver
+    git clone -b intel-media-22.5.3 --depth=1 https://github.com/intel/media-driver
     pushd media-driver
     sed -i 's|find_package(X11)||g' media_softlet/media_top_cmake.cmake media_driver/media_top_cmake.cmake
     mkdir build && pushd build
@@ -284,7 +285,7 @@ prepare_extra_amd64() {
 
     # Vulkan Headers
     pushd ${SOURCE_DIR}
-    git clone -b v1.3.224 --depth=1 https://github.com/KhronosGroup/Vulkan-Headers
+    git clone -b v1.3.226 --depth=1 https://github.com/KhronosGroup/Vulkan-Headers
     pushd Vulkan-Headers
     mkdir build && pushd build
     cmake \
@@ -297,7 +298,7 @@ prepare_extra_amd64() {
 
     # Vulkan ICD Loader
     pushd ${SOURCE_DIR}
-    git clone -b v1.3.224 --depth=1 https://github.com/KhronosGroup/Vulkan-Loader
+    git clone -b v1.3.226 --depth=1 https://github.com/KhronosGroup/Vulkan-Loader
     pushd Vulkan-Loader
     mkdir build && pushd build
     cmake \
@@ -344,7 +345,7 @@ prepare_extra_amd64() {
     # Minimal libs for AMD VAAPI, AMD RADV and Intel ANV
     if [[ $( lsb_release -c -s ) != "bionic" ]]; then
         # llvm >= 11
-        apt-get install -y llvm-11-dev
+        apt-get install -y llvm-11-dev libudev-dev
         pushd ${SOURCE_DIR}
         mkdir mesa
         pushd mesa
