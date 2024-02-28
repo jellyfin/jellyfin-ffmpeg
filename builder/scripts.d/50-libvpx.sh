a5e4d49678f37918c0eac3487bc25c0eee6b8b2d
gnattu <gnattuoc@me.com>
2024-02-28 17:31:07 +0800

    fix: missing configs

--- builder/scripts.d/50-libvpx.sh
+++ builder/scripts.d/50-libvpx.sh
@@ -12,7 +12,15 @@ ffbuild_dockerbuild() {
     cd libvpx
 
     local myconf=(
-        .
+        --disable-shared
+        --enable-static
+        --enable-pic
+        --disable-examples
+        --disable-tools
+        --disable-docs
+        --disable-unit-tests
+        --enable-vp9-highbitdepth
+        --prefix="$FFBUILD_PREFIX"
     )
 
     if [[ $TARGET == win64 ]]; then
