2bf215e8aef4227d97575d6ca4471b45ead02ad8
gnattu <gnattuoc@me.com>
2024-07-12 14:41:20 +0800

    ci: properly disable x11 on macos

--- builder/variants/defaults-mac.sh
+++ builder/variants/defaults-mac.sh
@@ -3,14 +3,18 @@ XCODE_MAJOR_VER="$(xcodebuild -version | grep 'Xcode' | awk '{print $2}' | cut -
 
 FF_CFLAGS+="-I"$FFBUILD_PREFIX"/include"
 FF_LDFLAGS+="-L"$FFBUILD_PREFIX"/lib"
-FF_CONFIGURE+=" --disable-libjack --disable-indev=jack --enable-neon --enable-runtime-cpudetect --enable-audiotoolbox --enable-videotoolbox"
+FF_CONFIGURE+=" --disable-libjack --disable-indev=jack --disable-libxcb --disable-xlib --enable-neon --enable-runtime-cpudetect --enable-audiotoolbox --enable-videotoolbox"
 FFBUILD_TARGET_FLAGS="--disable-shared --enable-static --pkg-config-flags=\"--static\" --enable-pthreads --cc=clang"
 FF_HOST_CFLAGS="-I/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include -I"$FFBUILD_PREFIX"/include"
 FF_HOST_LDFLAGS=""
-if [ $XCODE_MAJOR_VER -ge 15 ]; then
-  FF_HOST_LDFLAGS+="-Wl,-ld_classic "
-  export LDFLAGS="-Wl,-ld_classic"
-fi
+
+# This is no longer needed for latest xcode.
+# If you enconters duplicated symbol linker error try uncomment the following
+# if [ $XCODE_MAJOR_VER -ge 15 ]; then
+#   FF_HOST_LDFLAGS+="-Wl,-ld_classic "
+#   export LDFLAGS="-Wl,-ld_classic"
+# fi
+
 FF_HOST_LDFLAGS+="-L/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib -L"$FFBUILD_PREFIX"/lib"
 export PKG_CONFIG_LIBDIR="/usr/lib/pkgconfig:/opt/homebrew/Library/Homebrew/os/mac/pkgconfig/$MACOS_MAJOR_VER:/usr/local/Homebrew/Library/Homebrew/os/mac/pkgconfig/$MACOS_MAJOR_VER"
 export CMAKE_PREFIX_PATH=""$FFBUILD_PREFIX""
