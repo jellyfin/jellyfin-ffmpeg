d1b5c369662b14470562f79ed4ea9add0c9e66c8
gnattu <gnattuoc@me.com>
2025-02-01 15:10:46 +0800

    libxml2: pin stable version for windows build

--- docker-build-win64.sh
+++ docker-build-win64.sh
@@ -49,6 +49,8 @@ popd
 # LIBXML2
 git clone --depth=1 https://github.com/GNOME/libxml2.git
 pushd libxml2
+libxml2_ver="v2.13.5"
+git checkout ${libxml2_ver}
 # Fallback to internal entropy when system native method failed
 git apply ${SOURCE_DIR}/builder/patches/libxml2/0000-fallback-to-internal-entropy.patch
 ./autogen.sh \
