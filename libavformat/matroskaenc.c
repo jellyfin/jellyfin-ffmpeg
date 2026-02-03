443040c79a4e8f810a89cded7a153431e3ec7a1a
Till Fischer <tf@hmmc.io>
2026-02-03 22:48:51 +0100

    avformat/matroska: add support for VVC streams
    Backport VVC/H.266 Matroska container support from upstream FFmpeg.
    
    This enables playback of VVC-encoded video files in MKV containers,
    which was previously failing with "Unknown/unsupported AVCodecID V_MPEGI/ISO/VVC".
    
    Changes:
    - Add V_MPEGI/ISO/VVC codec tag mapping in matroska.c
    - Add VVC muxer support in matroskaenc.c (include, codec private, reformat)
    
    Specification: https://github.com/ietf-wg-cellar/matroska-specification/blob/master/codec_specs.md#v_mpegiisovvc
    
    Based on upstream FFmpeg commit:
    https://github.com/FFmpeg/FFmpeg/commit/df50370e1b10d39047fe92f7f4418094bb507eec
    
    Original author: James Almer <jamrial@gmail.com>

--- libavformat/matroskaenc.c
+++ libavformat/matroskaenc.c
@@ -26,6 +26,7 @@
 #include "av1.h"
 #include "avc.h"
 #include "hevc.h"
+#include "vvc.h"
 #include "avformat.h"
 #include "avio_internal.h"
 #include "avlanguage.h"
@@ -1140,6 +1141,9 @@ static int mkv_assemble_native_codecprivate(AVFormatContext *s, AVIOContext *dyn
                                   extradata_size);
     case AV_CODEC_ID_HEVC:
         return ff_isom_write_hvcc(dyn_cp, extradata,
+                                  extradata_size, 0);
+    case AV_CODEC_ID_VVC:
+        return ff_isom_write_vvcc(dyn_cp, extradata,
                                   extradata_size, 0);
     case AV_CODEC_ID_ALAC:
         if (extradata_size < 36) {
@@ -3441,8 +3445,10 @@ static int mkv_init(struct AVFormatContext *s)
             break;
         case AV_CODEC_ID_H264:
         case AV_CODEC_ID_HEVC:
+        case AV_CODEC_ID_VVC:
             if ((par->codec_id == AV_CODEC_ID_H264 && par->extradata_size > 0 ||
-                 par->codec_id == AV_CODEC_ID_HEVC && par->extradata_size > 6) &&
+                 par->codec_id == AV_CODEC_ID_HEVC && par->extradata_size > 6 ||
+                 par->codec_id == AV_CODEC_ID_VVC  && par->extradata_size >= 6) &&
                 (AV_RB24(par->extradata) == 1 || AV_RB32(par->extradata) == 1))
                 track->reformat = mkv_reformat_h2645;
             break;
